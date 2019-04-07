f2cdfa42ae1772ecede1ec358a324ec5bbad4b88
Indika Wijesooriya <indikman@gmail.com>
2019-04-07 19:13:53 +0530

    Initial Commit

---
+++ Assets/Scripts/Shaders/CurvedShaderController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CurvedShaderController : MonoBehaviour
+{
+
+    [Header("Offset")]
+    [SerializeField]
+    private Vector4 offset = new Vector4(0,0,0,0);
+
+    [SerializeField]
+    private float distance = 200;
+
+    [SerializeField]
+    private bool isDevelop = false;
+
+    public static void SetValues(Vector4 _offset, float _distance)
+    {
+        Shader.SetGlobalVector("_QOffset", _offset);
+        Shader.SetGlobalFloat("_Dist", _distance);
+    }
+
+#if !UNITY_EDITOR
+    private IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+        if(!isDevelop){
+            SetValues(offset, distance);
+        }else{
+            SetValues(new Vector4(0,0,0,0), 100);
+        }
+    }
+#endif
+
+#if UNITY_EDITOR
+    // Update is called once per frame
+    private void Update()
+    {
+        
+        if(!isDevelop){
+            SetValues(offset, distance);
+        }else{
+            SetValues(new Vector4(0,0,0,0), 100);
+        }
+    }
+#endif
+
+}
